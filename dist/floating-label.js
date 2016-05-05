d68624d31b54a114d8ad2503ee913eb91f856491
stephen gardner <stephen gardner>
2016-05-05 03:00:33 -0400

    test

--- dist/floating-label.js
+++ dist/floating-label.js
@@ -897,82 +897,82 @@ var divtag     = document.querySelector("div");
 		}
 
 		function postLink(scope, element, attr, ctrls){
-			// var containerCtrl = ctrls[0];
-			// var hasNgModel = !!ctrls[1];
-			// var ngModelCtrl = ctrls[1];
-			// var isReadonly = angular.isDefined(attr.readonly);
-			// if (!containerCtrl) return;
-			// if (containerCtrl.input) {
-			// 	throw new Error("<md-input-container> can only have *one* <input>, <textarea> or <md-select> child element!");
-			// }
+			var containerCtrl = ctrls[0];
+			var hasNgModel = !!ctrls[1];
+			var ngModelCtrl = ctrls[1];
+			var isReadonly = angular.isDefined(attr.readonly);
+			if (!containerCtrl) return;
+			if (containerCtrl.input) {
+				throw new Error("<md-input-container> can only have *one* <input>, <textarea> or <md-select> child element!");
+			}
 
 			// var isErrorGetter = containerCtrl.isErrorGetter || function() {
 			// 		console.log("Returning ", ngModelCtrl.$invalid + " && " + ngModelCtrl.$touched);
 			// 		return ngModelCtrl.$invalid && (ngModelCtrl.$touched || isParentFormSubmitted());
 			// 	};
-			// console.log("label is:", label);
-			// var isParentFormSubmitted = function () {
-			// 	var parent = false;//$mdUtil.getClosest(element, 'form');
-			// 	var form = parent ? angular.element(parent).controller('form') : null;
-			//
-			// 	return form ? form.$submitted : false;
-			// };
-			//
-			// var isErrorGetter = function() {
-			// 	// added ngModelCtrl.$dirty
-			// 	// $touched is only applied after exiting the input
-			// 	return containerCtrl.isErrorGetter
-			// 		|| (ngModelCtrl.$invalid && (ngModelCtrl.$touched/* || ngModelCtrl.$dirty*/));
-			// }
-			// scope.$watch(function(){
-			// 	return ngModelCtrl.$touched
-			// }, function(value) {
-			// 	// containerCtrl.setTouched(ngModelCtrl.$touched);
-			// })
-			// scope.$watch(isErrorGetter, containerCtrl.setInvalid);
-			// scope.$watch(isErrorGetter, containerCtrl.setInvalid);
+			console.log("label is:", label);
+			var isParentFormSubmitted = function () {
+				var parent = false;//$mdUtil.getClosest(element, 'form');
+				var form = parent ? angular.element(parent).controller('form') : null;
 
-			// var label = containerCtrl.element[0].querySelector('label');
-			// wrapInput(scope, element);
-			// scope.inputWrapper.prepend(label);
+				return form ? form.$submitted : false;
+			};
+
+			var isErrorGetter = function() {
+				// added ngModelCtrl.$dirty
+				// $touched is only applied after exiting the input
+				return containerCtrl.isErrorGetter
+					|| (ngModelCtrl.$invalid && (ngModelCtrl.$touched/* || ngModelCtrl.$dirty*/));
+			}
+			scope.$watch(function(){
+				return ngModelCtrl.$touched
+			}, function(value) {
+				containerCtrl.setTouched(ngModelCtrl.$touched);
+			})
+			// scope.$watch(isErrorGetter, containerCtrl.setInvalid);
+			scope.$watch(isErrorGetter, containerCtrl.setInvalid);
+
+			var label = containerCtrl.element[0].querySelector('label');
+			wrapInput(scope, element);
+			scope.inputWrapper.prepend(label);
 
 			// Add an error spacer div after our input to provide space for the char counter and any ng-messages
-			// var errorsSpacer = angular.element('<div class="ag-errors-spacer">');
-			// var inputGroup = containerCtrl.element[0].querySelector('.ag-input-group, .input-group');
-			// if(inputGroup) {
-			// 	angular.element(inputGroup).after(errorsSpacer);
-			// }
-			// else {
-			// 	scope.inputWrapper.after(errorsSpacer);
-			// }
+			var errorsSpacer = angular.element('<div class="ag-errors-spacer">');
+			var inputGroup = containerCtrl.element[0].querySelector('.ag-input-group, .input-group');
+			if(inputGroup) {
+				angular.element(inputGroup).after(errorsSpacer);
+			}
+			else {
+				scope.inputWrapper.after(errorsSpacer);
+			}
 
-			// ngModelCtrl.$parsers.push(ngModelPipelineCheckValue);
-			// ngModelCtrl.$formatters.push(ngModelPipelineCheckValue);
+			ngModelCtrl.$parsers.push(ngModelPipelineCheckValue);
+			ngModelCtrl.$formatters.push(ngModelPipelineCheckValue);
 
-			// function ngModelPipelineCheckValue(arg) {
-			// 	// containerCtrl.setHasValue(!ngModelCtrl.$isEmpty(arg));
-			// 	return arg;
-			// }
-			//
-			// containerCtrl.input = element;
-			// element.addClass('ag-input');
-			// element
-			// 	.on('focus', function(ev) {
-			// 		// $agUtil.nextTick(function() {
-			// 		// 	containerCtrl.setFocused(true);
-			// 		// });
-			// 	})
-			// 	.on('blur', function(ev) {
-			// 		// $agUtil.nextTick(function(){
-			// 		// 	containerCtrl.setFocused(false);
-			// 		// 	inputCheckValue();
-			// 		// });
-			// 	});
-			// function inputCheckValue() {
-			// 	// An input's value counts if its length > 0,
-			// 	// or if the input's validity state says it has bad input (eg string in a number input)
-			// 	containerCtrl.setHasValue(element.val().length > 0 || (element[0].validity || {}).badInput);
-			// }
+			function ngModelPipelineCheckValue(arg) {
+				containerCtrl.setHasValue(!ngModelCtrl.$isEmpty(arg));
+				return arg;
+			}
+
+			containerCtrl.input = element;
+			element.addClass('ag-input');
+			element
+				.on('focus', function(ev) {
+					$agUtil.nextTick(function() {
+						containerCtrl.setFocused(true);
+					});
+				})
+				.on('blur', function(ev) {
+					$agUtil.nextTick(function(){
+						containerCtrl.setFocused(false);
+						inputCheckValue();
+					});
+				});
+			function inputCheckValue() {
+				// An input's value counts if its length > 0,
+				// or if the input's validity state says it has bad input (eg string in a number input)
+				containerCtrl.setHasValue(element.val().length > 0 || (element[0].validity || {}).badInput);
+			}
 		}
 	}
 	// angular.module('agFloatingLabel')
@@ -1089,48 +1089,6 @@ var divtag     = document.querySelector("div");
 
 	angular.module('agFloatingLabel')
 		.directive('ngMessage', ngMessageDirective)
-})(window.angular);
-(function (angular) {
-	function placeholderDirective() {
-		return {
-			restrict: 'A',
-			require: '^^?agFloatingLabel',
-			priority: 200,
-			link: postLink
-		};
-
-		function postLink(scope, element, attr, agFloatingLabel) {
-			// If there is no input container, just return
-			console.log("postLink");
-			if (!agFloatingLabel) return;
-			console.log("...postLink");
-
-			var label = agFloatingLabel.element.find('label');
-			var hasNoFloat = angular.isDefined(agFloatingLabel.element.attr('ag-no-float'));
-
-			console.log("label:", label);
-			// If we have a label, or they specify the md-no-float attribute, just return
-			if ((label && label.length) || hasNoFloat) {
-				// Add a placeholder class so we can target it in the CSS
-				agFloatingLabel.setHasPlaceholder(true);
-				return;
-			}
-
-			// Otherwise, grab/remove the placeholder
-			var placeholderText = attr.placeholder;
-			element.removeAttr('placeholder');
-
-			// And add the placeholder text as a separate label
-			if (agFloatingLabel.input && agFloatingLabel.input[0].nodeName != 'MD-SELECT') {
-				var placeholder = '<label ng-click="delegateClick()">' + placeholderText + '</label>';
-
-				agFloatingLabel.element.addClass('ag-icon-float');
-				agFloatingLabel.element.prepend(placeholder);
-			}
-		}
-	}
-	angular.module('agFloatingLabel')
-		.directive('placeholder', placeholderDirective)
 })(window.angular);
 (function (angular) {
 
