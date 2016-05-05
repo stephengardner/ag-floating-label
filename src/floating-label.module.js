874513d4b2db7227c56575ce5114e1e041604a4b
stephen gardner <stephen gardner>
2016-05-05 06:37:53 -0400

    test

--- src/floating-label.module.js
+++ src/floating-label.module.js
@@ -2,6 +2,6 @@
     'use strict';
     var app = angular.module('agFloatingLabel', ['ngMessages', 'ngAnimate']);
 	app.config(function($animateProvider){
-		$animateProvider.classNameFilter(/^(?:(?!ng-animate-disabled).)*$/)
+		$animateProvider.classNameFilter(/^(?:(?!ng-animate-disabled).)*$/);
 	});
 })(window.angular);
